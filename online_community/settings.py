77c7f5a759120c8ae5fbdc8ed1736ab14d39aa11
isrealolalere <isrealolalere09@gmail.com>
2023-06-29 03:16:21 -0700

    added whitenoise and some other config

--- online_community/settings.py
+++ online_community/settings.py
@@ -52,6 +52,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,6 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #own config
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'learnshare/static/')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/learnshare/login'
